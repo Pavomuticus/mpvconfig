574a9b197efb10a28c348c0dfc263154e011cd43
Pavomuticus <89340536+Pavomuticus@users.noreply.github.com>
2022-11-18 21:42:02 +0800

    Update cymode.lua

--- portable_config/scripts/cymode.lua
+++ portable_config/scripts/cymode.lua
@@ -1,19 +1,16 @@
-mp.observe_property("playlist-count", "number", function(_, value)
-    if(value > 1) then mode = "3buttons" else mode = "2buttons" end
-end)
-mp.commandv('script-message-to', 'uosc', 'set', 'icon', "close")
+mp.commandv('script-message-to', 'uosc', 'set', 'icon', 'close')
 mp.register_script_message('set', function(prop, value)
     if value == "file" then
-        mp.set_property_native("loop-file", "inf")
-        mp.set_property_native("loop-playlist", "no")
-        mp.commandv('script-message-to', 'uosc', 'set', 'icon', "file")
-    elseif value == "playlist" and mode == "3buttons" then
-        mp.set_property_native("loop-file", "no")
-        mp.set_property_native("loop-playlist", "inf")
-        mp.commandv('script-message-to', 'uosc', 'set', 'icon', "playlist")
-    elseif value == "close" or value == "playlist" and mode == "2buttons" then
-        mp.set_property_native("loop-file", "no")
-        mp.set_property_native("loop-playlist", "no")
-        mp.commandv('script-message-to', 'uosc', 'set', 'icon', "close")
+        mp.set_property("loop-file", "inf")
+        mp.set_property("loop-playlist", "no")
+        mp.commandv('script-message-to', 'uosc', 'set', 'icon', 'file')
+    elseif value == "playlist" and mp.get_property_number('playlist-count') ~= 1 then
+        mp.set_property("loop-file", "no")
+        mp.set_property("loop-playlist", "inf")
+        mp.commandv('script-message-to', 'uosc', 'set', 'icon', 'playlist')
+    else
+        mp.set_property("loop-file", "no")
+        mp.set_property("loop-playlist", "no")
+        mp.commandv('script-message-to', 'uosc', 'set', 'icon', 'close')
     end
 end)
